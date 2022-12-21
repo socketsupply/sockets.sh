e0d2dd22cd54caf51024f8b47e35bf72a308ba9a
Rahim Bedjou <lamiabedjou2017@gmail.com>
2022-12-21 11:21:10 -0500

    fix-done

--- src/pages/config.js
+++ src/pages/config.js
@@ -19,8 +19,14 @@ class PageConfig extends Tonic {
           <main>
             <h1>Config File</h1>
             <p>
+<<<<<<< HEAD
               Each project needs a <code>socket.ini</code> file in the root
               direcrory. This is an <a href="https://en.wikipedia.org/wiki/INI_file">ini</a> file.
+=======
+              Each project needs a <code>ssc.config</code> file in the root
+              directory. This is a plain text file with key-value property pairs.
+              The file also supports comments.
+>>>>>>> 04bfad9 (fix-done)
             </p>
 
             <markdown-module src="src/docs/config.md"></markdown-module>
